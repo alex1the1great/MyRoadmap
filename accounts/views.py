2e4b1ec98f3ad77799bba723b452ad38a4fac63a
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-23 20:03:35 +0545

    feat: user can login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,7 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
