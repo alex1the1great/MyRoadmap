b6799c527637e1e8cf595e45b726d47dbbde7bb3
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-23 18:53:18 +0545

    add login and logout redirect url

--- roadmap_root/settings.py
+++ roadmap_root/settings.py
@@ -140,4 +140,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
