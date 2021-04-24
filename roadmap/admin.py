37910ac59682a5f0828fe5ab97295eb7860d99cd
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-24 08:56:02 +0545

    update the UI of goal model in admin site

--- roadmap/admin.py
+++ roadmap/admin.py
@@ -5,4 +5,7 @@ from .models import Goal
 
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'created_at']
+    list_display = ['title', 'user', 'created_at', 'deadline']
+    list_filter = ['user']
+    raw_id_fields = ('user',)
+    prepopulated_fields = {'slug': ('title',)}
