3004afd4377cb37b507011ae1ee50e715ee1b00d
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-24 16:28:08 +0545

    feat[roadmap]: list all goals and tasks

--- roadmap/views.py
+++ roadmap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from .models import Goal
+
+
+def goal_list(request):
+    goals = Goal.objects.all()
+    today_date = timezone.now()
+    return render(request, 'roadmap/goal_list.html', {'goals': goals, 'today_date': today_date})
--- roadmap_root/urls.py
+++ roadmap_root/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('goals/', include('roadmap.urls'))
 ]
