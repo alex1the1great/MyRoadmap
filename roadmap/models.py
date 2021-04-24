6dce87b1e8b1958da0262e8fe03dc7e7c738d833
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-24 08:42:10 +0545

    migration[accounts]: custom user model, create[roadmap]: goal model

--- roadmap/models.py
+++ roadmap/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Goal(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='created_at')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+
+    def __str__(self):
+        return self.title
