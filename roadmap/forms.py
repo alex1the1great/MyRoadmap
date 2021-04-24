090bc0c523190997da936b209f883b9fcc0bd01b
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-24 19:41:32 +0545

    add[roadmap]: custom date time widget

--- roadmap/forms.py
+++ roadmap/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from .models import Goal
 
 
+class CustomDateTimeInput(forms.DateTimeInput):
+    input_type = 'date'
+
+
 class GoalForm(forms.ModelForm):
     class Meta:
         model = Goal
         fields = ['title', 'deadline']
+        widgets = {
+            'deadline': CustomDateTimeInput()
+        }
