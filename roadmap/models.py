8b2c5298a3b6370f3c365b110ac478cb8edcbf4c
Asim Shrestha <asimshrestha608@gmail.com>
2021-04-24 09:49:58 +0545

    create[roadmap]: create validator for deadline

--- roadmap/models.py
+++ roadmap/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 User = settings.AUTH_USER_MODEL
+
+
+def validate_deadline_is_not_past(value):
+    if not value >= timezone.now():
+        raise ValidationError('Deadline should be in future date.')
 
 
 class Goal(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique_for_date='created_at')
-    deadline = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField(default=timezone.now, validators=[validate_deadline_is_not_past])
     created_at = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
